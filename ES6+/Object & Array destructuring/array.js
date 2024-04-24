1b90103e3bd72888396050c0e284f6a2326e2043
Harshit Sachan <harshitsachan99@gmail.com>
2024-04-24 22:56:44 +0530

    Sent

---
+++ ES6+/Object & Array destructuring/array.js
@@ -0,0 +1,40 @@
+
+//map, forEach, filter, reduce
+//Higher order functions
+//These properties/methods are works with arrays only.
+
+//map gives us a new array.
+//ForEach doesnt return anything.
+
+var arr = [13, 9, 46, 33, 7, 90, 21];
+
+//newArr = arr.forEach(a=>a+10)
+
+
+
+
+//filter is just takes in a condition//condition is true it returns
+
+//newArr = arr.filter((a,i)=>a>2);
+
+
+
+
+//Array creates in a many ways
+
+var arr1 = new Array(3).fill(6);
+var arr1 = new Array(1, 2, 3)
+console.log(arr1)
+
+
+//Diff between arrays & objects
+//arrays have index values pairs. // are special types of objects// another object//
+//Objects have key valus pairs
+console.log(typeof arr === typeof{})
+
+
+
+//2d arrays, mxn
+// Transpose of an array. mxn-> nxm
+
+
