cdbf587699ee835573f1b745d010c567c63f68ea
Harshit Sachan <harshitsachan99@gmail.com>
2024-05-06 15:29:13 +0530

    done

---
+++ ES6+/Object & Array destructuring/objects.js
@@ -0,0 +1,28 @@
+
+var key = '2'
+var key2 = '7'
+
+var obj={
+    key,key2
+}
+console.log(obj)
+
+
+var person = {}
+person.name = 'Jhon';
+person.email = 'jhon@gmail.com';
+person.location = 'India';
+
+var person2 = {
+    name: 'John2',
+    email: 'john@gmail.com',
+    location: 'India'
+}
+
+const name = person2.name
+const location = person.location
+
+console.log(name, location)
+
+
+
