d3983bea20c3cac625e2b2e9d90bc5ddd1ca7416
Harshit Sachan <harshitsachan99@gmail.com>
2024-05-18 17:09:12 +0530

    Done

---
+++ ES6+/Object & Array destructuring/functions.js
@@ -0,0 +1,23 @@
+
+function test(arg1, arg2){
+    //console.log(arguments)
+}
+test(1, 2)
+console.log(test)
+
+// var n = 2;
+// function square (num){
+//     var ans = num * num;
+//     return ans;
+// }
+// var sqaure2 = square(n);
+// var sqaure4 = square(4);
+
+getName();
+console.log(x);
+console.log(getName);
+
+var x = 7;
+function getName(){
+    console.log("hello harsh")
+}
