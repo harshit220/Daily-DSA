6cd75803193f8634a57166bca4f3440964346b95
Harshit Sachan <harshitsachan99@gmail.com>
2024-05-19 21:11:50 +0530

    done

---
+++ Practise/index.js
@@ -0,0 +1,26 @@
+
+
+// var x = 7;
+// function getName(){
+//     console.log("hello harsh")
+// }
+
+// getName();
+// console.log(x);
+// console.log(getName);
+
+var x = 1;
+a();
+b();
+console.log(x);
+
+function a(){
+    var x = 10;
+    console.log(x);
+}
+
+function b(){
+    var x = 100;
+    console.log(x);
+}
+
