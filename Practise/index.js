c9d0b5aebde4961dcde39522aeb1786f286eb2e8
Harshit Sachan <harshitsachan99@gmail.com>
2024-05-25 17:04:22 +0530

    done

--- Practise/index.js
+++ Practise/index.js
@@ -9,18 +9,56 @@
 // console.log(x);
 // console.log(getName);
 
-var x = 1;
-a();
-b();
-console.log(x);
 
-function a(){
-    var x = 10;
-    console.log(x);
+// var x = 1;
+// a();
+// b();
+// console.log(x);
+
+// function a(){
+//     var x = 10;
+//     console.log(x);
+// }
+
+// function b(){
+//     var x = 100;
+//     console.log(x);
+// }
+
+
+
+// function a(){
+//     // console.log(b);
+// c();
+//     function c(){
+//         console.log(b);
+//     }
+// }
+// var b = 10;
+// a();
+
+
+
+//Closure:-
+
+// function x(){
+//     var a = 9;
+//     function y(){
+//         console.log(a);
+//     }
+//     y();
+// }
+// x();
+
+function x (){
+    var a = 900;
+    function y(){
+        var b = 500;
+        function z(){
+            console.log(a, b);
+        }
+        z();
+    }
+    y();
 }
-
-function b(){
-    var x = 100;
-    console.log(x);
-}
-
+x();
